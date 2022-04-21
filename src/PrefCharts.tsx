7109f4ed2647bb582617c5b994224a03de0f02e1
ぶち <6uclz1@users.noreply.github.com>
2022-04-22 00:57:23 +0900

    Style change and refactor

--- src/PrefCharts.tsx
+++ src/PrefCharts.tsx
@@ -8,10 +8,11 @@ import {
     YAxis,
     CartesianGrid,
     Tooltip,
-    Legend
+    Legend,
+    ResponsiveContainer
   } from "recharts";
 
-const fetchCatFactsData = async (setData:any, props:any) => {
+const fetchCatFactsData = async (setGraphData:any, props:any) => {
     let prefData: any = {};
     for (const pref of props.prefList) {
         const res = await fetch('https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=' + pref.prefCode, {
@@ -24,81 +25,76 @@ const fetchCatFactsData = async (setData:any, props:any) => {
         const data = await res.json()
         prefData[pref.prefName] = data.result.data[0].data;
     }
-    setData(prefData)
+
+    const data: any = prefData;
+    let gDataList: any = [];
+    // let yearList: any = [];
+    const yearList: number[] = [1960, 1965, 1970, 1975, 1980, 1985, 1990, 1995, 2000, 2005, 2010, 2015, 2020, 2025, 2030, 2035, 2040, 2045];
+
+    // for (const [, value] of Object.entries(data)) {
+    //     // console.log(key);
+    //     // console.log(value);
+    //     const v:any = value;
+    //     yearList = v.map((v: { year: any; }) => v.year);
+    // }
+
+    for (const year of yearList) {
+        let populationObject:any = {year: year}
+        for (const [key, value] of Object.entries(data)) {
+            const k:any = key
+            const v:any = value;
+            let vv:any = v.filter((v: { year: any; }) => v.year === year);
+            populationObject = {...populationObject, [k]: vv[0].value}
+        }
+        gDataList.push(populationObject);
+    }
+    setGraphData(gDataList);
 }
 
 const PrefCharts = (props:any) => {
 
-    const [data, setData] = useState<any>([]);
     const [graphData, setGraphData] = useState<any>([]);
 
     useLayoutEffect(() => {
-        fetchCatFactsData(setData, props)
-      }, [props])
-
-    useLayoutEffect(() => {
-
-        // setData(a);
-        let gDataList:any = [];
-
-        let yearList: any = [];
-        // let yearList: any = [1960, 1965, 1970, 1975, 1980, 1985, 1990, 1995, 2000, 2005, 2010, 2015, 2020, 2025, 2030, 2035, 2040, 2045];
-
-        for (const [, value] of Object.entries(data)) {
-            // console.log(key);
-            // console.log(value);
-            const v:any = value;
-            yearList = v.map((v: { year: any; }) => v.year);
-        }
-
-        for (const year of yearList) {
-            let populationObject:any = {year: year}
-            for (const [key, value] of Object.entries(data)) {
-                const k:any = key
-                const v:any = value;
-                let vv:any = v.filter((v: { year: any; }) => v.year === year);
-                populationObject = {...populationObject, [k]: vv[0].value}
-            }
-            gDataList.push(populationObject);
-        }
-
-        setGraphData(gDataList);
-
-    },[props,data])
+        fetchCatFactsData(setGraphData, props)
+    }, [props])
 
     return (
         <div className='PrefCharts'>
-            <LineChart
-              width={1080}
-              height={720}
-              data={graphData}
-              margin={{
-                top: 30,
-                right: 40,
-                left: 30,
-                bottom: 10
-              }}
-            >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="year" height={30} padding={{ left: 30, right: 30 }} angle={-20} />
-            <YAxis padding={{ top: 20, bottom: 20 }} />
-            <Tooltip />
-            <Legend />
-            {Object.entries(props.prefList).map(([key, value]: any) => {
-                return (
-                    <Line
-                        type="linear"
-                        key={key}
-                        dataKey={value.prefName}
-                        strokeWidth={3}
-                        stroke={'#'+Math.floor(Math.random()*16777215).toString(16)}
-                        activeDot={{ r: 8 }}
-                    />
-                );
-            })
-            }
-            </LineChart>
+            <div className='PrefCharts-container'>
+            <ResponsiveContainer width={'100%'} height={600}>
+                <LineChart
+                    width={1200}
+                    height={600}
+                    data={graphData}
+                    margin={{top: 30, right: 40, left: 30, bottom: 10 }}
+                >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="year" height={30} padding={{ left: 30, right: 30 }} angle={-20} />
+                <YAxis padding={{ top: 20, bottom: 20 }} />
+                <Tooltip />
+                <Legend />
+                {Object.entries(props.prefList).map(([key, value]: any) => {
+                    return (
+                        <Line
+                            type="linear"
+                            key={key}
+                            dataKey={value.prefName}
+                            strokeWidth={3}
+                            stroke={'#'+Math.floor(Math.random()*16777215).toString(16)}
+                            activeDot={{ r: 8 }}
+                            isAnimationActive={false}
+                        />
+                    );
+                })
+                }
+                </LineChart>
+            </ResponsiveContainer>
+            </div>
+
         </div>
+
+
     )
 }
 
--- src/PrefCheckbox.tsx
+++ src/PrefCheckbox.tsx
@@ -1,21 +1,6 @@
 import React, {useState, useLayoutEffect, useCallback} from 'react'
 import PrefCharts from './PrefCharts';
-
-// const fetchCatFactsData = async (setData:any, props:any) => {
-//     let prefData: any = {};
-//     for (const pref of props.prefList) {
-//         const res = await fetch('https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=' + pref.prefCode, {
-//             method: 'GET',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//                 'X-API-KEY': 'FXpRNLpjvvMd3rOz9dZsANvhY5ScNLa9P4H86OLb'
-//             },
-//         });
-//         const data = await res.json()
-//         prefData[pref.prefName] = data.result.data[0].data;
-//     }
-//     setData(prefData)
-// }
+import "./PrefCheckbox.css";
 
 const PrefCheckbox = () => {
 
@@ -23,13 +8,6 @@ const PrefCheckbox = () => {
     const [selectPrefList, setSelectPrefList] = useState<any>([]);
 
     const [checkedValues, setCheckedValues] = useState<any>([]);
-
-    // const [data, setData] = useState<any>([]);
-    // const [graphData, setGraphData] = useState<any>([]);
-
-    // useLayoutEffect(() => {
-    //     fetchCatFactsData(setData, props)
-    //   }, [props])
 
     const handleChange = useCallback((e:any) => {
         if (checkedValues.includes(e.target.value)) {
@@ -59,34 +37,37 @@ const PrefCheckbox = () => {
         setSelectPrefList(prefList.filter( (prefList: {prefCode: any; prefName: any} ) => checkedValues.includes(prefList.prefName)));
     },[checkedValues, prefList])
 
-    useLayoutEffect(() => {
-        setSelectPrefList(selectPrefList);
-    },[selectPrefList])
-
     return (
         <>
+        <div className='PrefCheckbox'>
             <p>
                 現在選択されている値：<b>{checkedValues.join(", ")}</b>
             </p>
-            {prefList.length !== 0 &&
-                prefList.map((prefList: {
-                    prefCode: any; prefName: any
-                }) =>
-                    <label key={prefList.prefName}>
-                        <input
-                            key={prefList.prefName}
-                            type="checkbox"
-                            value={prefList.prefName}
-                            onChange={handleChange}
-                            checked={checkedValues.includes(prefList.prefName)}
-                        />
-                        {prefList.prefName}
-                    </label>
-                )
-            }
-            <PrefCharts
-                prefList={selectPrefList}
-            />
+            <div className='PrefCheckbox-container'>
+                {prefList.length !== 0 &&
+                    prefList.map((prefList: {
+                        prefCode: any; prefName: any
+                    }) =>
+                        <div className='Checkbox'>
+                            <label key={prefList.prefName}>
+                            <input
+                                key={prefList.prefName}
+                                type="checkbox"
+                                value={prefList.prefName}
+                                onChange={handleChange}
+                                checked={checkedValues.includes(prefList.prefName)}
+                            />
+                            {prefList.prefName}
+                            </label>
+                        </div>
+
+                    )
+                }
+            </div>
+        </div>
+        <PrefCharts
+            prefList={selectPrefList}
+        />
         </>
       )
 }
