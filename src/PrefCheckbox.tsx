84353b4d79e852fce0ec27cb9587d4e1f1e2a4da
ぶち <6uclz1@users.noreply.github.com>
2022-04-23 01:19:32 +0900

    Add linter settings

--- src/PrefCheckbox.tsx
+++ src/PrefCheckbox.tsx
@@ -1,76 +1,76 @@
-import React, {useState, useLayoutEffect, useCallback} from 'react'
-import PrefCharts from './PrefCharts';
+import React, { useState, useLayoutEffect, useCallback } from "react";
+import PrefCharts from "./PrefCharts";
 import "./PrefCheckbox.css";
 
 const PrefCheckbox = () => {
+  type PrefType = {
+    prefCode: number;
+    prefName: string;
+  };
+  const [prefList, setPrefList] = useState<Array<PrefType>>([]);
+  const [selectPrefList, setSelectPrefList] = useState<Array<PrefType>>([]);
 
-    const [prefList, setPrefList] = useState<any>([]);
-    const [selectPrefList, setSelectPrefList] = useState<any>([]);
+  const [checkedValues, setCheckedValues] = useState<Array<String>>([]);
 
-    const [checkedValues, setCheckedValues] = useState<any>([]);
+  const handleChange = useCallback(
+    (e: any) => {
+      if (checkedValues.includes(e.target.value)) {
+        setCheckedValues(
+          checkedValues.filter(
+            (checkedValue: String) => checkedValue !== e.target.value
+          )
+        );
+      } else {
+        setCheckedValues([...checkedValues, e.target.value]);
+      }
+    },
+    [checkedValues]
+  );
 
-    const handleChange = useCallback((e:any) => {
-        if (checkedValues.includes(e.target.value)) {
-            const a = checkedValues.filter((checkedValue:any) => checkedValue !== e.target.value)
-            setCheckedValues(a);
-          } else {
-            const a = [...checkedValues, e.target.value]
-            setCheckedValues(a);
-          }
-    }, [checkedValues]);
+  useLayoutEffect(() => {
+    fetch("https://opendata.resas-portal.go.jp/api/v1/prefectures", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "X-API-KEY": "FXpRNLpjvvMd3rOz9dZsANvhY5ScNLa9P4H86OLb",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setPrefList(data.result);
+      });
+  }, []);
 
-    useLayoutEffect(() => {
-        fetch('https://opendata.resas-portal.go.jp/api/v1/prefectures', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-API-KEY': 'FXpRNLpjvvMd3rOz9dZsANvhY5ScNLa9P4H86OLb'
-              },
-          })
-        .then(res => res.json())
-        .then(data => {
-            setPrefList(data.result)
-        })
-    },[])
+  useLayoutEffect(() => {
+    setSelectPrefList(
+      prefList.filter((prefList) => checkedValues.includes(prefList.prefName))
+    );
+  }, [checkedValues, prefList]);
 
-    useLayoutEffect(() => {
-        setSelectPrefList(prefList.filter( (prefList: {prefCode: any; prefName: any} ) => checkedValues.includes(prefList.prefName)));
-    },[checkedValues, prefList])
-
-    return (
-        <>
-        <div className='PrefCheckbox'>
-            {/* <p>
-                現在選択されている値：<b>{checkedValues.join(", ")}</b>
-            </p> */}
-            <div className='PrefCheckbox-container'>
-                {prefList.length !== 0 &&
-                    prefList.map((prefList: {
-                        prefCode: any; prefName: any
-                    }) =>
-                        <div className='Checkbox'>
-                            <label key={prefList.prefName}>
-                            <input
-                                key={prefList.prefName}
-                                type="checkbox"
-                                value={prefList.prefName}
-                                onChange={handleChange}
-                                checked={checkedValues.includes(prefList.prefName)}
-                            />
-                            {prefList.prefName}
-                            </label>
-                        </div>
-
-                    )
-                }
-            </div>
+  return (
+    <>
+      <div className="PrefCheckbox">
+        <div className="PrefCheckbox-container">
+          {prefList.length !== 0 &&
+            prefList.map((prefList) => (
+              <div className="Checkbox" key={prefList.prefName}>
+                <label key={prefList.prefName}>
+                  <input
+                    key={prefList.prefName}
+                    type="checkbox"
+                    value={prefList.prefName}
+                    onChange={handleChange}
+                    checked={checkedValues.includes(prefList.prefName)}
+                  />
+                  {prefList.prefName}
+                </label>
+              </div>
+            ))}
         </div>
-        <PrefCharts
-            prefList={selectPrefList}
-        />
-        </>
-      )
-}
-
+      </div>
+      <PrefCharts prefList={selectPrefList} />
+    </>
+  );
+};
 
 export default PrefCheckbox;
