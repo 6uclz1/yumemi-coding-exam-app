ac666180becddc40e7ca0ed85077737dd866c0d4
ぶち <6uclz1@users.noreply.github.com>
2022-04-24 04:55:05 +0900

    [test] App.js Test fix.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+it("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement =
+    screen.getByText(/表示したい都道府県を選択してください。/i);
   expect(linkElement).toBeInTheDocument();
 });
