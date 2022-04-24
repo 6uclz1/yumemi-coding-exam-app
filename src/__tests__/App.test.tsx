c6129b15e10fa6d1d403b737229b993dfb5361c7
ぶち <6uclz1@users.noreply.github.com>
2022-04-24 15:24:17 +0900

    [Refactor] Move app.test file

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import App from "../App";
 it("renders info message.", () => {
   render(<App />);
   const linkElement =
