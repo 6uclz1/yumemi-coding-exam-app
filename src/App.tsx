1c9666a209ffc31b4dbd8a469c659bd2e58d1f12
ぶち <6uclz1@users.noreply.github.com>
2022-04-24 19:30:51 +0900

    Add description and footer components.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from "react";
 
 import PrefCheckbox from "./components/PrefCheckbox";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
@@ -10,9 +11,13 @@ export default function App() {
       <Header />
       <div className="App">
         <div className="App-container">
+          <div className="description">
+            RESAS(地域経済分析システム)に掲載されている、人口構成のデータをグラフに表示します。
+          </div>
           <PrefCheckbox />
         </div>
       </div>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,6 @@
+import "./Footer.css";
+import React from "react";
+
+export default function Footer() {
+  return <div className="Footer"># Created by 6uclz1.</div>;
+}
