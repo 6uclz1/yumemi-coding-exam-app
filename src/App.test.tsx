b0957a0f3488fb0e23af627ffc2b4637bb5a57e3
ぶち <6uclz1@users.noreply.github.com>
2022-04-24 13:53:07 +0900

    [TEST] Wrong test push.
    Error Handle test.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
-it("renders learn react link", () => {
+it("renders info message.", () => {
   render(<App />);
   const linkElement =
     screen.getByText(/表示したい都道府県を選択してください。/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+it("wrong test.", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/wrong test./i);
   expect(linkElement).toBeInTheDocument();
 });
