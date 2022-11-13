07781d0e4f8bae4c70adef3446a7960ecdabb776
ぶち <6uclz1@users.noreply.github.com>
2022-11-13 17:35:03 +0900

    Fix test.
    mock window.ResizeObserver.

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "../App";
-it.skip("renders info message.", () => {
-  render(<App />);
-  const linkElement =
-    screen.getByText(/表示したい都道府県を選択してください。/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe("App", () => {
+  test("renders App component", () => {
+    window.ResizeObserver =
+      window.ResizeObserver ||
+      jest.fn().mockImplementation(() => ({
+        disconnect: jest.fn(),
+        observe: jest.fn(),
+      }));
+
+    render(<App />);
+    const linkElement =
+      screen.getByText(/表示したい都道府県を選択してください。/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
